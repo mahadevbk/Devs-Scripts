78b937477ef9d8568d956d15f786d5e5b1984895
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-05-10 15:30:38 +0400

    Update devs-scripts.py

--- devs-scripts.py
+++ devs-scripts.py
@@ -75,7 +75,7 @@ apps = [
         "name": "Mini Tournament Organiser",
         "url": "https://tournament-organiser.streamlit.app/",
         "desc": "One Day mini tournament organiser with PDF output.",
-       "icon": "/static/court.png"
+       "icon": "https://github.com/mahadevbk/Devs-Scripts/blob/main/static/court.png"
     },
 ]
 
