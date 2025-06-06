62520c9f8974aa7cd55aadeeccdd2831ee7394fd
mahadevbk <68096457+mahadevbk@users.noreply.github.com>
2025-06-06 19:39:58 +0400

    Update devs-scripts.py

--- devs-scripts.py
+++ devs-scripts.py
@@ -19,7 +19,7 @@ h4, div, button {
 """, unsafe_allow_html=True)
 
 st.markdown("""
-<h1 style="font-family: 'Baumans'; font-size: 3.5rem; margin-bottom: 0.3em; display: flex; align-items: center; gap: 14px;">
+<h1 style="font-family: 'Baumans'; font-size: 3.25rem; margin-bottom: 0.3em; display: flex; align-items: center; gap: 14px;">
     <img src="https://raw.githubusercontent.com/mahadevbk/Devs-Scripts/main/static/devgreyround.png" alt="Logo" style="height: 60px;"/>
            Dev's     Streamlit     App     Hub  🧰
 </h1>
